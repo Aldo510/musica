3f4bc6b7c6bca3b43a5c9aa8ff7c2eb891fb460e
Aldo Marquez <aldo.619_@ciencias.unam.mx>
2016-11-04 19:46:43 -0600

    App and Angular added to index

---
+++ app.js
@@ -0,0 +1,12 @@
+//Ifee
+//El modulo por default  ya no se vuelve a tocar
+(function(){
+  'use strict';
+
+  angular
+  //ejemplo de poner dependencia, si queremos mas de una se separan con coma
+    .module("musica", ["ngResource"]);
+
+
+
+})();//fin funcion de autenticacion
